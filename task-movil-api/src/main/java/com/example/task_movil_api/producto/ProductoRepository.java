6c8dff7b93260c33f8bee73ff1725979b59946a8
Juan Sebastian Osorio Fierro <jsosorio-2032b@corhuila.edu.co>
2025-10-25 15:36:40 -0500

    Dejar main vacía

--- task-movil-api/src/main/java/com/example/task_movil_api/producto/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.task_movil_api.producto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    
-}
